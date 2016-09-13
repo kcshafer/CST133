f4bbce427c44cc81a27bbe5df3277964c872271b
kcshafer <kcplusplus@live.com>
2016-09-12 22:00:41 -0400

    finished ex 1

--- ShaferKEx1/ShaferKEx1.cpp
+++ ShaferKEx1/ShaferKEx1.cpp
@@ -37,13 +37,13 @@ int main(void)
 	ofstream outputFile;
 	outputFile.open("output.txt");
 
-	//task 1a variables
+	//task variables
 	int firstNumber;
 	int secondNumber;
-
-	//task 1b variables
 	int index;
 	int counter;
+	int squareRoot;
+	int squareRootSum;
 
 	//start heading to console
 	cout << setfill('-') << setw(DIVIDER_WIDTH) << ' ' << setfill(' ') << endl;
@@ -120,7 +120,7 @@ int main(void)
 		//read first number and second number from input
 		cout << "Please input two integers where the first integer is less than the second: " << endl;
 		cin >> firstNumber >> secondNumber;
-		
+
 		//check if the firstNumber is less than the secondNumber
 		if (firstNumber > secondNumber)
 		{
@@ -144,13 +144,26 @@ int main(void)
 	cout << setfill('-') << setw(DIVIDER_WIDTH) << ' ' << setfill(' ') << endl;
 	cout << setfill(' ') << setw((stringLength + DIVIDER_WIDTH) / 2) << ' ' << setfill(' ') << endl;
 
-	counter = 1;
+	//write title to output file
+	outputFile << setfill('-') << setw(DIVIDER_WIDTH) << ' ' << setfill(' ') << endl;
+	outputFile << taskTitle << endl;
+
+	//line break after title
+	outputFile << "" << endl;
+
+	//write start and end number to file
+	outputFile << "Start Value:         " << firstNumber << setfill(' ') << "     End Value:         " << secondNumber << endl;
+
+	outputFile << setfill('-') << setw(DIVIDER_WIDTH) << ' ' << setfill(' ') << endl;
+
+
+	counter = 0;
 	for (index = firstNumber; index < secondNumber; index++)
 	{
 		//check if number is odd
 		if (index % 2 == 1)
 		{
-			cout << index << " ";
+			outputFile << index << " ";
 
 			//increment counter only if it's odd
 			counter++;
@@ -159,18 +172,72 @@ int main(void)
 		//check if we have 5 numbers and if we do, print that line out
 		if (counter == 5)
 		{
-			counter = 1;
-			cout << endl;
+			counter = 0;
+			outputFile << endl;
 		}
 	}
 
 	//end solution for task 1b
 
+	//solution for task 1d
 
+	//while loop for 1d
+	taskTitle = "task 1d - while loop";
+	outputFile << setfill('-') << setw(DIVIDER_WIDTH) << ' ' << setfill(' ') << endl;
+	outputFile << taskTitle << endl;
+	outputFile << "" << endl;
+	outputFile << setw(10) << "Number" << setw(10) << "Square" << endl;
+	outputFile << setfill('-') << setw(DIVIDER_WIDTH) << ' ' << setfill(' ') << endl;
 
+	//reusing counter 
+	counter = 1;
+	while (counter <= 10)
+	{
+		//square the counter
+		squareRoot = counter * counter;
+		outputFile << setw(10) << counter << setw(10) << squareRoot << endl;
 
+		counter++;
+	}
 
+	//for loop for 1d
+	taskTitle = "task 1d - for loop";
+	outputFile << setfill('-') << setw(DIVIDER_WIDTH) << ' ' << setfill(' ') << endl;
+	outputFile << taskTitle << endl;
+	outputFile << "" << endl;
+	outputFile << setw(10) << "Number" << setw(10) << "Square" << endl;
+	outputFile << setfill('-') << setw(DIVIDER_WIDTH) << ' ' << setfill(' ') << endl;
 
+	//for loop for squares, reuses index for the loop variable
+	for (index = 1; index <= 10; index++)
+	{
+		//square the counter
+		squareRoot = index * index;
+		outputFile << setw(10) << index << setw(10) << squareRoot << endl;
+	}
+
+	//end solution for task 1d
+
+	//solution for task 1e
+	taskTitle = "task 1e - for loop";
+	outputFile << setfill('-') << setw(DIVIDER_WIDTH) << ' ' << setfill(' ') << endl;
+	outputFile << taskTitle << endl;
+	outputFile << "" << endl;
+	outputFile << setw(10) << "Number" << setw(10) << "Square" << setw(10) << "Running Sum" << endl;
+	outputFile << setfill('-') << setw(DIVIDER_WIDTH) << ' ' << setfill(' ') << endl;
+
+	squareRootSum = 0;
+	for (index = firstNumber; index < secondNumber; index++)
+	{
+		if (index % 2 != 0)
+		{
+			squareRoot = index * index;
+			squareRootSum += squareRoot;
+			outputFile << setw(10) << index << setw(10) << squareRoot << setw(10) << squareRootSum << endl;
+		}
+	}
+
+	//end solution for task 1e
 
 	//close output file
 	outputFile.close();
