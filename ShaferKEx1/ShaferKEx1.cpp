9e909875da28b13f9adb9f357c37afed9c1a4906
kcshafer <kcplusplus@live.com>
2016-09-20 20:02:32 -0400

    catching up the rest of the files

--- ShaferKEx1/ShaferKEx1.cpp
+++ ShaferKEx1/ShaferKEx1.cpp
@@ -1,12 +1,169 @@
-//----------------------------------------------------------------
-//Programmer's Name: KC Shafer
-//Program: Exercise 1
-//Program Flow (IPO):
-//TODO
+//-----------------------------------------------------------------------------
+//Name: KC Shafer
+//Program:  Exercise 1
 //Grade:
-//Comments:
-//----------------------------------------------------------------
-
+//General Comments:  
+//                   
+//                   
+//-----------------------------------------------------------------------------
+//Met Standard Programming Requirements
+// Students should review this PDF file to assure these specs are met
+//  which can be found in Angel in Important Course Documents
+// There is a summary of the program requirements doc that can be
+// found in the lab link in Angel
+// 
+// Comments for each category of the Programming Standards:
+//  Project: 
+//   
+//  Program Order:
+//   
+//  Main Doc Block:
+//
+//  Identifiers:
+//   
+//  Style Requirements:
+//   
+//  Points Lost -------------------------------------------------> 
+//
+//-----------------------------------------------------------------------------
+//Program Specifications
+//
+//General Exercise Requirements
+//  Heading
+//    Includes the required course heading output to the output file
+//      centered
+//    Includes the college, your name, course & Exercise number
+//    Heading printed to the screen and to the output file
+//  Divider
+//    One line output divider used to separate and block any output
+//    to the file or the screen
+//
+//Comment:
+//  
+//  
+//  
+//Points lost -----------------------------------------------------------> 
+//
+//Task 1a
+//  Efficient solution for the loop type
+//  Solved using a while loop FIRST
+//  Solved a second time using a do-while loop
+//  while loop
+//    uses input with a primer and a changer
+//  do while
+//   user input statement ONLY ONCE in the loop
+//  Error message printed on screen if invalid user input
+//  values from each loop stored in the SAME variables
+//  values from second loop used in later processing
+//  Output
+//   Output to the screen includes:
+//     Task # & loop type, prompt & user input, error message where 
+//        appropriate
+//   Output to the file includes:
+//     Task # and loop type
+//     Two valid values eventually entered from each loop execution
+//Comment:
+//  
+//  
+//  
+//Points lost -----------------------------------------------------------> 
+//
+//Task 1b
+//  Efficient solution for the loop type
+//  Solved using a for loop ONLY
+//  Values used are from Task 1A do while loop
+//  Output
+//    Task # and loop type
+//    Inclusive of start and end range value output to the output file
+//    Output of numbers in 5 formatted & aligned columns
+//Comment:
+//  
+//  
+//  
+//Points lost -----------------------------------------------------------> 
+//
+//Task 1d
+//  Efficient solution for the loop type
+//  Solved using a while loop FIRST
+//  Solved a second time using a for loop
+//  Output
+//    Task # and loop type for EACH type
+//    Formatted column headings using setw
+//    Each pair - number & square output on a separate line
+//Comment:
+//  
+//  
+//  
+//Points lost -----------------------------------------------------------> 
+//
+//Task 1e
+//  Efficient solution for the loop type
+//  Solved using a for loop
+//  Output
+//    ONLY ODD values included in the output
+//    Task # & loop type
+//    Formatted column headings using setw
+//    Aligned output of number, square and running total
+//      each set of values on one line in the output file
+//Comment:
+//  
+//  
+//  
+//Points lost -----------------------------------------------------------> 
+//
+//Miscellaneous errors not anticipated by your professor
+//  
+//Comment:
+//  
+//  
+//Points lost -----------------------------------------------------------> 
+//
+//-----------------------------------------------------------------------------
+//Total Possible Points for the Lab -------------------------------------> 100
+//Points Lost ----------------------------------------------------------->  
+//Lab Grade ------------------------------------------------------------->  
+//-----------------------------------------------------------------------------
+//
+//Documentation Block
+//Exercise 1
+//Open the logical output file fout for Ex1Output.txt
+//Output the course heading to the screen and to the output file
+//Task 1a
+//IPO:
+//Input two integer values from the keyboard
+//While the first is less than or equal to the second:
+// Output an error message to the screen stating first is less than or
+//  or equal to the second
+//  Input two integer values from the keyboard
+// Output success message to the screen
+// Output the two values to the output file
+//
+//Task 1b
+//IPO:
+//For each value from the first value to the second value
+// from the previous task:
+//  Determine if the value is odd,
+//   Output the value to the output file
+//   Determine if five values have been output to the file
+//     move to the next line in the output file.
+//
+//Task 1d
+//IPO:
+//Output the values from 1 to 10 along with their squares to the output file
+//
+//Task 1e
+//IPO:
+//Initialize the accumulator variable
+//For each value entered by the user from the first value
+//  to the second value:
+//  Determine if the value is odd
+//    Calculate the square of the value
+//    Accumulate the square in the sum of squares
+//  Output the value along with its square, and
+//   the running sum of the squares to the output file
+//
+//Close the logical output file for Ex1Output.txt
+//-----------------------------------------------------------------------------
 //for input and output on the screen
 #include <iostream>
 
