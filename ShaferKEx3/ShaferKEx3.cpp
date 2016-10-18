8a27e0a3c617f1792d862dc53d927f79531eb174
kcshafer <kcplusplus@live.com>
2016-10-18 10:14:25 -0400

    finished lab except for documentation

--- ShaferKEx3/ShaferKEx3.cpp
+++ ShaferKEx3/ShaferKEx3.cpp
@@ -151,16 +151,20 @@ using namespace std;
 
 void outputDivider(int numberOfSymbols, char symbol);
 void outputCourseHeading();
-int* getGrades(int& grade1, int &grade2, int& grade3, int& grade4);
+void outputResultsHeading();
+void outputQuizResults(int grade1, int grade2, int grade3, int grade4, double average, int highest, int lowest, int aboveAvgCount);
+void getGrades(int& grade1, int &grade2, int& grade3, int& grade4);
+void getHighestAndLowest(int& grade1, int& grade2, int& grade3, int& grade4, int& lowest, int& highest);
+int countGradesAboveAverage(int grade1, int grade2, int grade3, int grade4, int average);
 double getAverage(int grade1, int grade2, int grade3, int grade4);
+
+const string NAME = "KC Shafer";
+const string TITLE = "Fall 2016 Quiz Grades";
 
 int main()
 {
 	//set cout precision
 	cout.precision(4);
-
-
-	int* grades;
 
 	//variables for holding the quiz grades
 	int grade1;
@@ -171,18 +175,33 @@ int main()
 	//variable to store the calculated average
 	double average;
 
+	//variables to store the calculated lowest and highest
+	int lowest;
+	int highest;
+
+	//variable to store count above average
+	int aboveAvgCount;
+
 	//output the standard course heading to the screen
 	outputCourseHeading();
 
 	//prompt the user for the four grades and store them
-	grades = getGrades(grade1, grade2, grade3, grade4);
+	getGrades(grade1, grade2, grade3, grade4);
 
-	cout << "Grades: " << grades;
+	//output divider between entered grades and results
+	outputDivider(75, '-');
 
 	//calculate the average of the four grades
 	average = getAverage(grade1, grade2, grade3, grade4);
 
-	cout << average << endl;
+	//get the highest and lowest quiz grades
+	getHighestAndLowest(grade1, grade2, grade3, grade4, lowest, highest);
+
+	//get the number of grades above average
+	aboveAvgCount = countGradesAboveAverage(grade1, grade2, grade3, grade4, average);
+
+	//output quiz results and calculations
+	outputQuizResults(grade1, grade2, grade3, grade4, average,highest, lowest, aboveAvgCount );
 
 	return 0;
 }
@@ -204,14 +223,10 @@ void outputCourseHeading()
 	outputDivider(75, '-');
 }
 
-int* getGrades(int& grade1, int &grade2, int& grade3, int& grade4)
+void getGrades(int& grade1, int &grade2, int& grade3, int& grade4)
 {
 	cout << "Enter four quiz grades: " << endl;
 	cin >> grade1 >> grade2 >> grade3 >> grade4;
-	
-	int grades[4] = { grade1, grade2, grade3, grade4 };
-
-	return grades;
 }
 
 double getAverage(int grade1, int grade2, int grade3, int grade4)
@@ -220,7 +235,113 @@ double getAverage(int grade1, int grade2, int grade3, int grade4)
 	double average;
 	
 	//calculate the average
-	average = (grade1 + grade2 + grade3 + grade4) / (double)4;
+	average = (double)(grade1 + grade2 + grade3 + grade4) / (double)4;
 
 	return average;
+}
+
+void outputResultsHeading()
+{
+	cout << NAME << endl;
+	cout << TITLE << endl;
+
+	outputDivider(75,'-');
+}
+
+void outputQuizResults(int grade1, int grade2, int grade3, int grade4, double average, int highest, int lowest, int aboveAvgCount)
+{
+	//output the result heading
+	outputResultsHeading();
+
+	//output all four grades
+	cout << "Grade 1: " << grade1 << endl;
+	cout << "Grade 2: " << grade2 << endl;
+	cout << "Grade 3: " << grade3 << endl;
+	cout << "Grade 4: " << grade4 << endl;
+
+	//output the average
+	cout << fixed << setprecision(3);
+	cout << "Average: " << average << endl;
+
+	//output the count above average
+	cout << "Number of grades above the average: " << aboveAvgCount << endl;
+
+	//output the highest and lowest grades
+	cout << "Lowest Grade: " << lowest << endl;
+	cout << "Highest Grade: " << highest << endl;
+}
+
+void getHighestAndLowest(int& grade1, int& grade2, int& grade3, int& grade4, int& lowest, int& highest)
+{
+	//start with grade1 as highest and lowest
+	lowest = grade1;
+	highest = grade1;
+
+	//check if grade2 is lower than lowest
+	if (grade2 < lowest)
+	{
+		lowest = grade2;
+	}
+
+	//check if grade2 is higher than highest
+	if (grade2 > highest)
+	{
+		highest = grade2;
+	}
+
+	//check if grade3 is lower than lowest
+	if (grade3 < lowest)
+	{
+		lowest = grade3;
+	}
+
+	//check if grade3 is higher than highest
+	if (grade3 > highest)
+	{
+		highest = grade3;
+	}
+
+	//check if grade4 is lower than lowest
+	if (grade4 < lowest)
+	{
+		lowest = grade4;
+	}
+
+	//check if grade4 is higher than highest
+	if (grade4 > highest)
+	{
+		highest = grade4;
+	}
+}
+
+int countGradesAboveAverage(int grade1, int grade2, int grade3, int grade4, int average)
+{
+	int averageCount;
+	averageCount = 0;
+
+	//check if grade1 is above average
+	if (grade1 > average)
+	{
+		averageCount++;
+	}
+
+	//check if grade2 is above average
+	if (grade2 > average)
+	{
+		averageCount++;
+	}
+
+	//check if grade3 is above average
+	if (grade3 > average)
+	{
+		averageCount++;
+	}
+
+	//check if grade4 is above average
+	if (grade4 > average)
+	{
+		averageCount++;
+	}
+
+	return averageCount;
 }
